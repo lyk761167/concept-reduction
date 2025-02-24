979f031af5ac89cbba8aa724dc32afd965493668
lyk761167 <139837676+lyk761167@users.noreply.github.com>
2025-02-24 21:50:57 +0800

    Add files via upload

---
+++ 文字有概念约简 (2)/djangoProject/app01/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import matrix_view, process_matrix
+from app01 import views
+from .views import save_accuracy
+urlpatterns = [
+    path('', views.matrix_view, name='matrix_view'),  # 主页
+    path('process_matrix/', views.process_matrix, name='process_matrix'),  # 处理矩阵的 URL
+    path('save_accuracy/', save_accuracy, name='save_accuracy'),
+    path('orm/', views.orm),
+
+]
